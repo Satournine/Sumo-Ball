5100ee603a8ba7bf4aeb863b0453758248eb3de9
Satournine <32436768+Satournine@users.noreply.github.com>
2021-06-09 21:41:08 +0300

    init

---
+++ BallRoll/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    GameObject focalPoint;
+    private Rigidbody playerRb;
+    public float speed = 5f;
+    float forwardInput;
+    public bool hasPowerup;
+    private float forcePower = 15.0f;
+    public GameObject powerupIndicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.3f, 0);
+
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            powerupIndicator.gameObject.SetActive(true);
+            StartCoroutine(PowerupCountdownRoutine());
+
+        }
+    }
+    IEnumerator PowerupCountdownRoutine()
+    {
+        print("Power Up Activated");
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidBody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRigidBody.AddForce(awayFromPlayer * forcePower, ForceMode.Impulse);
+
+        }
+    }
+
+}
---
+++ BallRoll/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+    private float spawnRange = 9f;
+    int enemyCount;
+    int powerupCount;
+    int waveEvemies = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemyWaveSpawn(waveEvemies);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+    }
+
+    private void EnemyWaveSpawn(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        powerupCount = FindObjectsOfType<PowerUp>().Length;
+
+        if(enemyCount == 0)
+        {
+            waveEvemies += 2;
+            EnemyWaveSpawn(waveEvemies);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+    }
+
+    IEnumerator PowerUpExisenz()
+    {
+        if (powerupCount > 2)
+        {
+            yield return new WaitForSeconds(5);
+            Destroy(gameObject);
+        }
+        
+
+    }
+
+private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 enemySpawnPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return enemySpawnPos;
+    }
+
+}
